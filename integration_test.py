2de865793bcd13d330d6a40a4d41e4003bb2ece0
tyhchoi <imthomaschoi@gmail.com>
2017-02-05 17:09:24 -0800

    Modified Integration test to serve files

--- integration_test.py
+++ integration_test.py
@@ -1,9 +1,17 @@
 from subprocess import *
 from sys import exit
+import os
 
 def exit_test(server, return_val):
     server.kill()
-    exit(return_val) 
+    exit(return_val)
+
+def expected_output(output, expected):
+    if (output != expected):
+        print('FAILED: Output does not match expected')
+        print('Expected: ' + expected.decode('utf-8'))
+        print('Output: ' + out.decode('utf-8'))
+        exit_test(server, 1)
 
 def main():
     print('Building Webserver...')
@@ -14,16 +22,27 @@ def main():
     print('Starting Webserver...')
     server = Popen(['./Webserver', 'config'], stdout=DEVNULL)
 
-    print('Sending request...')
+    print('Sending echo request...')
     out = check_output(["curl", "-s", "localhost:8080/echo_request"])
     expected = b'GET /echo_request HTTP/1.1\r\nUser-Agent: curl/7.35.0\r\nHost: localhost:8080\r\nAccept: */*\r\n\r\n';
 
-    print('Checking output...')
-    if (out != expected):
-        print('FAILED: Output does not match expected')
-        print('Expected: ' + expected.decode('utf-8'))
-        print('Output: ' + out.decode('utf-8'))
-        exit_test(server, 1)
+    print('Checking echo output...')
+    expected_output(out, expected)
+
+    if not os.path.exists('example_files'):
+        os.makedirs('example_files')
+
+    path = 'example_files/asdf.txt'
+    fh = open(path,'w')
+    fh.write('asdf')
+    fh.close();
+
+    print('Sending file request...')
+    out = check_output(["curl", "-s", "localhost:8080/static/asdf.txt"])
+    expected = b'asdf';
+
+    print('Checking file output...')
+    expected_output(out, expected)
 
     print('SUCCESS')
     exit_test(server, 0)
