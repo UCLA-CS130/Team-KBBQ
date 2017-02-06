9f62a0cd300a7c85c50cdb8a504f635ce411072c
tyhchoi <imthomaschoi@gmail.com>
2017-02-05 18:51:15 -0800

    added parameter in expected_output

--- integration_test.py
+++ integration_test.py
@@ -6,7 +6,7 @@ def exit_test(server, return_val):
     server.kill()
     exit(return_val)
 
-def expected_output(output, expected):
+def expected_output(output, expected, server):
     if (output != expected):
         print('FAILED: Output does not match expected')
         print('Expected: ' + expected.decode('utf-8'))
@@ -27,7 +27,7 @@ def main():
     expected = b'GET /echo_request HTTP/1.1\r\nUser-Agent: curl/7.35.0\r\nHost: localhost:8080\r\nAccept: */*\r\n\r\n';
 
     print('Checking echo output...')
-    expected_output(out, expected)
+    expected_output(out, expected, server)
 
     if not os.path.exists('example_files'):
         os.makedirs('example_files')
@@ -42,7 +42,7 @@ def main():
     expected = b'asdf';
 
     print('Checking file output...')
-    expected_output(out, expected)
+    expected_output(out, expected, server)
 
     print('SUCCESS')
     exit_test(server, 0)
